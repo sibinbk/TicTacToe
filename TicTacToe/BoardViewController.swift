f2c0c18386539fd605a76583e796a88f97e85552
Sibin <sibinbk@gmail.com>
2016-07-19 17:51:57 +1000

    Comparison logic completed

--- TicTacToe/BoardViewController.swift
+++ TicTacToe/BoardViewController.swift
@@ -12,23 +12,27 @@ private let reuseIdentifier = "BoardCell"
 
 class BoardViewController: UICollectionViewController {
 
-    var boardSize = 3
-    var gameArray = [[String?]]()
+    enum Player: Int {
+        case Player1 = 0
+        case Player2 = 1
+    }
+    
+    let boardSize = 4
+    var gameArray = [[String]]()
+    var gameFininshed = false
+    var turnCount = 0
+    var currentPlayer = Player.Player1
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Resize Cells to fit in the view
         let width = CGRectGetWidth(collectionView!.frame) / CGFloat(boardSize)
         let layout = collectionViewLayout as! UICollectionViewFlowLayout
         layout.itemSize = CGSize(width: width, height: width)
         
-        for _ in 0..<boardSize {
-            var rowArray = [String?]()
-            for _ in 0..<boardSize {
-                rowArray.append(nil)
-            }
-            gameArray.append(rowArray)
-        }
+        // Preload array with nil.
+        reInitialiseBoard()
     }
 
     override func didReceiveMemoryWarning() {
@@ -42,7 +46,6 @@ class BoardViewController: UICollectionViewController {
         return boardSize
     }
 
-
     override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return boardSize
     }
@@ -52,9 +55,8 @@ class BoardViewController: UICollectionViewController {
         
         cell.boxLabel.text = ""
         
-        if let cellValue = gameArray[indexPath.section][indexPath.row] {
-            cell.boxLabel.text = cellValue
-        }
+        let cellValue = gameArray[indexPath.section][indexPath.row]
+        cell.boxLabel.text = cellValue
         
         return cell
     }
@@ -62,7 +64,102 @@ class BoardViewController: UICollectionViewController {
     // MARK: UICollectionViewDelegate
     
     override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
-        gameArray[indexPath.section][indexPath.row] = "X"
+        drawMarkForSpot(indexPath, player: currentPlayer)
         collectionView.reloadItemsAtIndexPaths([indexPath])
+    }
+    
+    func drawMarkForSpot(indexPath: NSIndexPath, player: Player) {
+        let playerMark = player == .Player1 ? "X" : "O"
+        gameArray[indexPath.section][indexPath.row] = playerMark
+        checkForWinForRow(indexPath.section, column: indexPath.row)
+        switchTurn()
+    }
+    
+    func checkForWinForRow(row: Int, column: Int) {
+        var rowCheckWon = true
+        var columnCheckWon = true
+        var leftDiagonalWon = true
+        var rightDiagonalWon = true
+        
+        for column in 0..<boardSize-1 {
+            let first = gameArray[row][column]
+            let second = gameArray[row][column + 1]
+            if first != "" && first == second {
+                continue
+            } else {
+                rowCheckWon = false
+                break
+            }
+        }
+        
+        for row in 0..<boardSize-1 {
+            let first = gameArray[row][column]
+            let second = gameArray[row+1][column]
+            if first != "" && first == second {
+                continue
+            } else {
+                columnCheckWon = false
+                break
+            }
+        }
+        
+        for i in 0..<boardSize-1 {
+            let first = gameArray[i][i]
+            let second = gameArray[i+1][i+1]
+            if first != "" && first == second {
+                continue
+            } else {
+                leftDiagonalWon = false
+                break
+            }
+        }
+        
+        for i in 0..<boardSize-1 {
+            let first = gameArray[boardSize-1-i][i]
+            let second = gameArray[boardSize-2-i][i+1]
+            if first != "" && first == second {
+                continue
+            } else {
+                rightDiagonalWon = false
+                break
+            }
+        }
+
+        if rowCheckWon || columnCheckWon || leftDiagonalWon || rightDiagonalWon {
+            print("\(currentPlayer) has won")
+            gameFininshed = true
+            reInitialiseBoard()
+        } else {
+            print("Not done yet")
+        }
+    }
+    
+    func switchTurn() {
+        if gameFininshed {
+            return
+        }
+        
+        turnCount += 1
+        currentPlayer = currentPlayer == .Player1 ? .Player2 : .Player1
+    }
+    
+    func reInitialiseBoard() {
+        // Empty all cells
+        for row in 0..<boardSize {
+            var rowArray = [String]()
+            for column in 0..<boardSize {
+                rowArray.insert("", atIndex: column)
+            }
+            gameArray.insert(rowArray, atIndex: row)
+        }
+        // Reset current player
+        currentPlayer = .Player1
+        turnCount = 0
+        gameFininshed = false
+        collectionView?.reloadData()
+    }
+    
+    @IBAction func resetGame(sender: AnyObject) {
+        reInitialiseBoard()
     }
 }
