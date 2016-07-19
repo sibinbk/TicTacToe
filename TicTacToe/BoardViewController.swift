bc5137725b3318e68337e5958cd52b222c55d076
Sibin <sibinbk@gmail.com>
2016-07-20 05:28:11 +1000

    Final Commit

--- TicTacToe/BoardViewController.swift
+++ TicTacToe/BoardViewController.swift
@@ -31,7 +31,6 @@ class BoardViewController: UICollectionViewController, BoardSizeSelectionDelegat
     var gameFininshed = false
     var takenCellCount = 0
     var currentPlayer = Player.Player1
-//    var winner = WhoWon.Tie
     var infoLabel: UILabel!
     
     override func viewDidLoad() {
@@ -53,44 +52,160 @@ class BoardViewController: UICollectionViewController, BoardSizeSelectionDelegat
         let layout = collectionViewLayout as! UICollectionViewFlowLayout
         layout.itemSize = CGSize(width: width, height: width)
         
+        // Set UI Elements on top of view.
+        setUpTurnInfoLabel()
+        setUpResetButton()
+        setUpGdridSelectionButton()
+        setupGameHistoryButton()
+        setUpResetHistoryButton()
+        
+        // Preload array with empty cell items.
+        initialiseBoardValues()
+    }
+    
+    func setUpTurnInfoLabel() {
         // Info label to display who's turn
-        infoLabel = UILabel(frame: CGRect(origin: CGPoint(x: 0, y: view.bounds.size.height*3/4), size: CGSize(width: view.bounds.size.width, height: 100)))
+        infoLabel = UILabel(frame: CGRect(origin: CGPoint(x: 0, y: view.bounds.size.width + 74), size: CGSize(width: view.bounds.size.width, height: 40)))
         infoLabel.backgroundColor = UIColor.whiteColor()
-        infoLabel.textColor = UIColor(red: 7/255, green: 185/255, blue: 155/255, alpha: 1)
-        infoLabel.font = UIFont.boldSystemFontOfSize(30)
+        infoLabel.textColor = UIColor(red: 52/255, green: 152/255, blue: 219/255, alpha: 1)
+        infoLabel.font = UIFont.boldSystemFontOfSize(24)
         infoLabel.textAlignment = NSTextAlignment.Center
         infoLabel.autoresizingMask = UIViewAutoresizing.FlexibleWidth
         let player = currentPlayer == .Player1 ? "Player 1" : "Player 2"
         infoLabel.text = "\(player) to move"
         view.addSubview(infoLabel)
+    }
+    
+    func setUpResetButton() {
+        let button = UIButton()
+        button.setTitle("Reset Game", forState: UIControlState.Normal)
+        button.backgroundColor = UIColor(red: 7/255, green: 185/255, blue: 155/255, alpha: 1)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 4
         
-        let player1Label = UILabel(frame: CGRect(origin: CGPoint(x: 20, y: view.bounds.size.height - 40), size: CGSize(width: view.bounds.size.width/2, height: 40)))
-        player1Label.backgroundColor = UIColor.whiteColor()
-        player1Label.textColor = UIColor(red: 7/255, green: 185/255, blue: 155/255, alpha: 1)
-        player1Label.font = UIFont.boldSystemFontOfSize(24)
-        player1Label.textAlignment = NSTextAlignment.Left
-        player1Label.autoresizingMask = UIViewAutoresizing.FlexibleWidth
-        player1Label.text = "Player 1 : X"
-        view.addSubview(player1Label)
+        button.addTarget(self, action: #selector(BoardViewController.resetGame(_:)), forControlEvents: .TouchUpInside)
         
-        let player2Label = UILabel(frame: CGRect(origin: CGPoint(x: view.bounds.size.width/2 - 20, y: view.bounds.size.height - 40), size: CGSize(width: view.bounds.size.width/2, height: 40)))
-        player2Label.backgroundColor = UIColor.whiteColor()
-        player2Label.textColor = UIColor(red: 7/255, green: 185/255, blue: 155/255, alpha: 1)
-        player2Label.font = UIFont.boldSystemFontOfSize(24)
-        player2Label.textAlignment = NSTextAlignment.Right
-        player2Label.autoresizingMask = UIViewAutoresizing.FlexibleWidth
-        player2Label.text = "Player 2 : O"
-        view.addSubview(player2Label)
-
-        // Preload array with empty cell items.
-        initialiseBoardValues()
+        self.view.addSubview(button)
+        
+        let heightConstraint = NSLayoutConstraint(item: button, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 40)
+        button.addConstraint(heightConstraint)
+        
+        let widthConstraint = NSLayoutConstraint(item: button, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 140)
+        button.addConstraint(widthConstraint)
+        
+        let centerXConstraint = NSLayoutConstraint(item: button, attribute: .CenterX, relatedBy: .Equal, toItem: view, attribute: .CenterX, multiplier: 1.0, constant: 80)
+        self.view.addConstraint(centerXConstraint)
+        
+        let bottomConstraint = NSLayoutConstraint( item: button, attribute: .Bottom, relatedBy: .Equal, toItem: view, attribute: .Bottom, multiplier: 1, constant: -70)
+        self.view.addConstraint(bottomConstraint)
+    }
+    
+    func setUpGdridSelectionButton() {
+        let button = UIButton()
+        button.setTitle("Select Grid", forState: UIControlState.Normal)
+        button.backgroundColor = UIColor(red: 7/255, green: 185/255, blue: 155/255, alpha: 1)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 4
+        
+        button.addTarget(self, action: #selector(BoardViewController.selectGridSize(_:)), forControlEvents: .TouchUpInside)
+        
+        self.view.addSubview(button)
+        
+        let heightConstraint = NSLayoutConstraint(item: button, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 40)
+        button.addConstraint(heightConstraint)
+        
+        let widthConstraint = NSLayoutConstraint(item: button, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 140)
+        button.addConstraint(widthConstraint)
+        
+        let centerXConstraint = NSLayoutConstraint(item: button, attribute: .CenterX, relatedBy: .Equal, toItem: view, attribute: .CenterX, multiplier: 1.0, constant: -80)
+        self.view.addConstraint(centerXConstraint)
+        
+        let bottomConstraint = NSLayoutConstraint( item: button, attribute: .Bottom, relatedBy: .Equal, toItem: view, attribute: .Bottom, multiplier: 1, constant: -70)
+        self.view.addConstraint(bottomConstraint)
+    }
+    
+    func setupGameHistoryButton() {
+        let button = UIButton()
+        button.setTitle("Game History", forState: UIControlState.Normal)
+        button.backgroundColor = UIColor(red: 7/255, green: 185/255, blue: 155/255, alpha: 1)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 4
+        
+        button.addTarget(self, action: #selector(BoardViewController.showPlayHistory(_:)), forControlEvents: .TouchUpInside)
+        
+        self.view.addSubview(button)
+        
+        let heightConstraint = NSLayoutConstraint(item: button, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 40)
+        button.addConstraint(heightConstraint)
+        
+        let widthConstraint = NSLayoutConstraint(item: button, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 140)
+        button.addConstraint(widthConstraint)
+        
+        let centerXConstraint = NSLayoutConstraint(item: button, attribute: .CenterX, relatedBy: .Equal, toItem: view, attribute: .CenterX, multiplier: 1.0, constant: 80)
+        self.view.addConstraint(centerXConstraint)
+        
+        let bottomConstraint = NSLayoutConstraint( item: button, attribute: .Bottom, relatedBy: .Equal, toItem: view, attribute: .Bottom, multiplier: 1, constant: -20)
+        self.view.addConstraint(bottomConstraint)
+    }
+    
+    func setUpResetHistoryButton() {
+        let button = UIButton()
+        button.setTitle("Delete History", forState: UIControlState.Normal)
+        button.backgroundColor = UIColor(red: 7/255, green: 185/255, blue: 155/255, alpha: 1)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 4
+        
+        button.addTarget(self, action: #selector(BoardViewController.deleteGameHistory(_:)), forControlEvents: .TouchUpInside)
+        
+        self.view.addSubview(button)
+        
+        let heightConstraint = NSLayoutConstraint(item: button, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 40)
+        button.addConstraint(heightConstraint)
+        
+        let widthConstraint = NSLayoutConstraint(item: button, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 140)
+        button.addConstraint(widthConstraint)
+        
+        let centerXConstraint = NSLayoutConstraint(item: button, attribute: .CenterX, relatedBy: .Equal, toItem: view, attribute: .CenterX, multiplier: 1.0, constant: -80)
+        self.view.addConstraint(centerXConstraint)
+        
+        let bottomConstraint = NSLayoutConstraint( item: button, attribute: .Bottom, relatedBy: .Equal, toItem: view, attribute: .Bottom, multiplier: 1, constant: -20)
+        self.view.addConstraint(bottomConstraint)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK : - User Action methods.
+    
+    func resetGame(sender: UIButton) {
+        initialiseBoardValues()
+    }
+    
+    func selectGridSize(sender: UIButton) {
+        performSegueWithIdentifier(gridSegueIdentifier, sender: nil)
+    }
+    
+    func showPlayHistory(sender: UIButton) {
+        performSegueWithIdentifier(historySegueIdentifier, sender: nil)
+    }
+    
+    func deleteGameHistory(sender: UIButton) {
+        let alertController = UIAlertController(title: "Delete History!", message: "Are you sure?", preferredStyle: .Alert)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        let okAction = UIAlertAction(title: "Yes", style: .Default) { (action:UIAlertAction!) in
+            self.resetSavedData()
+        }
+        alertController.addAction(okAction)
+        
+        self.presentViewController(alertController, animated: true, completion:nil)
 
+    }
+    
     // MARK: UICollectionViewDataSource
 
     override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
@@ -248,6 +363,7 @@ class BoardViewController: UICollectionViewController, BoardSizeSelectionDelegat
         defaults.setInteger(gameHistory.player1WinCount, forKey: "Player1WinCount")
         defaults.setInteger(gameHistory.player2WinCount, forKey: "Player2WinCount")
         defaults.setInteger(gameHistory.tieCount, forKey: "TieCount")
+        defaults.synchronize()
     }
     
     func loadGameHistory() -> GameHistory {
@@ -258,6 +374,18 @@ class BoardViewController: UICollectionViewController, BoardSizeSelectionDelegat
         let history = GameHistory(player1WinCount: player1Count, player2WinCount: player2Count, tieCount: tieCount)
         return history
     }
+    
+    func resetSavedData() {
+        // Reset game history object.
+        gameHistory.player1WinCount = 0
+        gameHistory.player2WinCount = 0
+        gameHistory.tieCount = 0
+        
+        NSUserDefaults.standardUserDefaults().removePersistentDomainForName(NSBundle.mainBundle().bundleIdentifier!)
+        NSUserDefaults.standardUserDefaults().synchronize()
+    }
+    
+
     
     func initialiseBoardValues() {
         loadGameCellArray()
@@ -280,18 +408,6 @@ class BoardViewController: UICollectionViewController, BoardSizeSelectionDelegat
             }
             gameArray.insert(rowArray, atIndex: row)
         }
-    }
-    
-    @IBAction func resetGame(sender: AnyObject) {
-        initialiseBoardValues()
-    }
-    
-    @IBAction func selectGridSize(sender: AnyObject) {
-        performSegueWithIdentifier(gridSegueIdentifier, sender: nil)
-    }
-    
-    @IBAction func showPlayHistory(sender: AnyObject) {
-        performSegueWithIdentifier(historySegueIdentifier, sender: nil)
     }
     
     // MARK: - Navigation
